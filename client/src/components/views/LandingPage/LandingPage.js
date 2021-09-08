c6be05aa84d5c8b9f6f96bcb071c0df56f60c2c2
sosow0212 <sosow0212@naver.com>
2021-09-08 18:57:57 +0900

    #2

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import MainImage from "./Sections/MainImage";
+import GridCards from "../commons/GridCards";
+import { Row } from "antd";
 // npm run dev
 
 function LandingPage() {
@@ -22,19 +24,32 @@ function LandingPage() {
   return (
     <div style={{ width: "100%", margin: "0" }}>
       {/* Main Image */}
-      {MainMovieImage && 
+      {MainMovieImage && (
         <MainImage
           image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
           title={MainMovieImage.original_title}
           text={MainMovieImage.overview}
         />
-      }
+      )}
 
       <div style={{ width: "85%", margin: "1rem auto" }}>
         <h2>Movies by latest</h2>
         <hr />
 
         {/* Movie Grid Cards */}
+        <Row gutter={[16,16]}>
+          {Movies &&
+            Movies.map((movie, index) => (
+              <React.Fragment key={index}>
+                <GridCards 
+                  image={movie.poster_path ?
+                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                    movieId={movie.id}
+                    movieName={movie.original_title}
+                />
+              </React.Fragment>
+            ))}
+        </Row>
       </div>
 
       <div style={{ display: "flex", justifyContent: "center" }}>
