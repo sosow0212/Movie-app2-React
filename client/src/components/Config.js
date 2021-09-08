7631bdc2e0c09c897ffb957a1a2f6fb0f2005e66
sosow0212 <sosow0212@naver.com>
2021-09-08 18:03:55 +0900

    #1

---
+++ client/src/components/Config.js
@@ -0,0 +1,12 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '350845cf70e40ec99617b06f725509cf';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+// Movie db API
+// 350845cf70e40ec99617b06f725509cf
+
+// 읽기 예
+// https://api.themoviedb.org/3/movie/550?api_key=350845cf70e40ec99617b06f725509cf
---
+++ server/config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongoURI:'mongodb+srv://user:<user>@reactstudy.cpeb7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+
+// Movie db API
+// 350845cf70e40ec99617b06f725509cf
+
+// 읽기 예
+// https://api.themoviedb.org/3/movie/550?api_key=350845cf70e40ec99617b06f725509cf
