8f61f9732e7d47b2c1094d04df1a0e923fa7b2ec
sosow0212 <sosow0212@naver.com>
2021-09-09 18:30:35 +0900

    #6.4 Make a Button

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
+import { Button } from "antd";
 
 function Favorite(props) {
   const movieId = props.movieId;
@@ -8,43 +9,68 @@ function Favorite(props) {
   const moviePost = props.movieInfo.backdrop_path;
   const movieRunTime = props.movieInfo.runtime;
 
-  const [FavoriteNumber, setFavoriteNumber] = useState(0)
-  const [Favorited, setFavorited] = useState(false)
+  const [FavoriteNumber, setFavoriteNumber] = useState(0);
+  const [Favorited, setFavorited] = useState(false);
 
+  let variables = {
+    userFrom,
+    movieId,
+    movieTitle,
+    moviePost,
+    movieRunTime,
+  };
+  console.log("1", FavoriteNumber);
   useEffect(() => {
-    let variables = {
-      userFrom,
-      movieId,
-    };
-
-    Axios.post("/api/favorite/favoriteNumber", variables)
-    .then((response) => {
+    Axios.post("/api/favorite/favoriteNumber", variables).then((response) => {
+      setFavoriteNumber(response.data.FavoriteNumber);
       if (response.data.success) {
-        setFavoriteNumber(response.data.FavoriteNumber)
       } else {
         alert("숫자 정보를 가져오는데 실패 했습니다.");
       }
     });
 
-
-    Axios.post("/api/favorite/favorited", variables)
-    .then((response) => {
+    Axios.post("/api/favorite/favorited", variables).then((response) => {
       if (response.data.success) {
-        setFavorited(response.data.Favorited)
-
+        setFavorited(response.data.Favorited);
       } else {
         alert("정보를 가져오는데 실패 했습니다.");
       }
     });
-
-
-
-
+    console.log("2", FavoriteNumber);
   }, []);
+
+  const onClickFavorite = () => {
+    if (Favorited) {
+      Axios.post("/api/favorite/removeFromFavorite", variables).then(
+        (response) => {
+          if (response.data.success) {
+            setFavoriteNumber(FavoriteNumber - 1);
+            console.log(FavoriteNumber);
+            setFavorited(!Favorited);
+          } else {
+            alert("Favorite 리스트에서 지우는 것을 실패했습니다.");
+          }
+        }
+      );
+    } else {
+      Axios.post("/api/favorite/addToFavorite", variables).then((response) => {
+        if (response.data.success) {
+          setFavoriteNumber(FavoriteNumber + 1);
+          console.log(FavoriteNumber);
+
+          setFavorited(!Favorited);
+        } else {
+          alert("Favorite 리스트에서 추가하는 것을 실패했습니다.");
+        }
+      });
+    }
+  };
 
   return (
     <div>
-      <button>{Favorited? "Not Favorite " : "Add to Favorite "} {FavoriteNumber}</button>
+      <Button onClick={onClickFavorite}>
+        {Favorited ? " Not Favorite" : "Add to Favorite "} {FavoriteNumber}
+      </Button>
     </div>
   );
 }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,19 +1,16 @@
-const { User } = require('../models/User');
+const { User } = require("../models/User");
 
-let auth = (req, res, next) => {
-  let token = req.cookies.w_auth;
-
-  User.findByToken(token, (err, user) => {
+const auth = (req, res, next) => {
+  const token = req.cookies.x_auth;
+  jwt.verify(token, "secretToken", (err, decoded) => {
     if (err) throw err;
-    if (!user)
-      return res.json({
-        isAuth: false,
-        error: true
-      });
-
-    req.token = token;
-    req.user = user;
-    next();
+    User.findOne({ _id: decoded, token: token }, (err, user) => {
+      if (err) return res.json({ success: false, err });
+      if (!user) return res.json({ isAuth: false, error: true });
+      req.token = token;
+      req.user = user;
+      next();
+    });
   });
 };
 
