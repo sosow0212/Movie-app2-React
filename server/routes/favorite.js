a28c33a40e1f5f81cf9353aa7b917cc3f0a5d0b3
sosow0212 <sosow0212@naver.com>
2021-09-09 17:51:04 +0900

    #6.1 Make a button

---
+++ server/routes/favorite.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { Favorite } = require("../models/Favorite");
+
+router.post("/favoriteNumber", (req, res) => {
+  req.body.movieId;
+
+  // mongoDB에서 favorite 숫자를 가져오기
+  Favorite.find({ movieId: req.body.movieId }).exec((err, info) => {
+    if (err) return res.status(400).send(err);
+
+    res.status(200).json({ success: true, favoriteNumber: info.length });
+  });
+});
+
+module.exports = router;
